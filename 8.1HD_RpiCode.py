befddbcf31cc10ea5d6bce859ac22795206228ec
thachoden <minhthac1511@gmail.com>
2025-05-19 03:23:36 +1000

    Update 8.1HD_RpiCode.py

--- 8.1HD_RpiCode.py
+++ 8.1HD_RpiCode.py
@@ -4,31 +4,59 @@ import RPi.GPIO as GPIO
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
 RED_PIN = 22
-GREEN_PIN = 27
-BLUE_PIN = 17
 
 # Set up the GPIO pins as outputs
 GPIO.setup(RED_PIN, GPIO.OUT)
-GPIO.setup(GREEN_PIN, GPIO.OUT)
-GPIO.setup(BLUE_PIN, GPIO.OUT)
 
-def off_all_led():
-    GPIO.output(RED_PIN, GPIO.LOW)
-    GPIO.output(GREEN_PIN, GPIO.LOW)
-    GPIO.output(BLUE_PIN, GPIO.LOW)
+# Set up PWM for each LED
+led_pwm = GPIO.PWM(RED_PIN, 100)   # 100 Hz
+led_pwm.start(0)   # Start with 0% duty cycle
+def off_led():
+    led_pwm.ChangeDutyCycle(0)
 def handle_led(distance):
-    if distance < 5:
-        off_all_led()
-        GPIO.output(RED_PIN, GPIO.HIGH)
-    elif distance >= 5 and distance <10:
-        off_all_led()
-        GPIO.output(GREEN_PIN, GPIO.HIGH)
-        GPIO.output(RED_PIN, GPIO.HIGH)
-    elif distance >=10 and distance < 15:
-        off_all_led()
-        GPIO.output(GREEN_PIN, GPIO.HIGH)
+    if distance < 0.5:
+        led_pwm.ChangeDutyCycle(100)  # 0.5 cm
+    elif distance < 1.0:
+        led_pwm.ChangeDutyCycle(95)    # 1.0 cm
+    elif distance < 1.5:
+        led_pwm.ChangeDutyCycle(90)    # 1.5 cm
+    elif distance < 2.0:
+        led_pwm.ChangeDutyCycle(85)    # 2.0 cm
+    elif distance < 2.5:
+        led_pwm.ChangeDutyCycle(80)    # 2.5 cm
+    elif distance < 3.0:
+        led_pwm.ChangeDutyCycle(75)    # 3.0 cm
+    elif distance < 3.5:
+        led_pwm.ChangeDutyCycle(70)    # 3.5 cm
+    elif distance < 4.0:
+        led_pwm.ChangeDutyCycle(65)    # 4.0 cm
+    elif distance < 4.5:
+        led_pwm.ChangeDutyCycle(60)    # 4.5 cm
+    elif distance < 5.0:
+        led_pwm.ChangeDutyCycle(55)    # 5.0 cm
+    elif distance < 5.5:
+        led_pwm.ChangeDutyCycle(50)
+    elif distance < 6.0:
+        led_pwm.ChangeDutyCycle(45)
+    elif distance < 6.5:
+        led_pwm.ChangeDutyCycle(40)
+    elif distance < 7.0:
+        led_pwm.ChangeDutyCycle(35)
+    elif distance < 7.5:
+        led_pwm.ChangeDutyCycle(30)
+    elif distance < 8.0:
+        led_pwm.ChangeDutyCycle(25)
+    elif distance < 8.5:
+        led_pwm.ChangeDutyCycle(20)
+    elif distance < 9.0:
+        led_pwm.ChangeDutyCycle(15)
+    elif distance < 9.5:
+        led_pwm.ChangeDutyCycle(10)
+    elif distance < 10.0:
+        led_pwm.ChangeDutyCycle(5)
     else:
-        off_all_led()
+        off_led()  
+
 def main():
     target_device = "ec:62:60:81:3c:da"
     # Connect to device
@@ -61,4 +89,8 @@ def main():
         peripheral.disconnect()
 
 if __name__ == "__main__":
+    try:
         main()
+    except Exception as e:
+        print(f"{e}")
+        GPIO.cleanup()
